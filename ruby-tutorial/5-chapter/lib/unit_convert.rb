ca32dfc1b2462d6a2bd55951606351237301a298
ChikaraHanakawa <hanakawa.c@gmail.com>
2024-01-25 22:30:24 +0900

    test

---
+++ ruby-tutorial/5-chapter/lib/unit_convert.rb
@@ -0,0 +1,28 @@
+def convert_length(num, before_unit, after_unit)
+    case before_unit
+        when 'm'
+            if after_unit == 'in'
+                num = num * 39.37
+                return num
+            elsif after_unit == 'ft'
+                num = num * 3,28
+                return num
+            end
+        when 'ft'
+            if after_unit == 'm'
+                num = num / 3.28
+                return num
+            elsif after_unit == 'in'
+                num = num * 12
+                return num
+            end
+        when 'in'
+            if after_unit == 'm'
+                num = num / 39.37
+                return num
+            elsif after_unit == 'ft'
+                num = num / 0.083333
+                return num
+            end
+    end
+end
