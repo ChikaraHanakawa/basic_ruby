9ec94b36e9179b985afe5b57e0f5f67fa448d571
ChikaraHanakawa <hanakawa.c@gmail.com>
2024-02-03 10:16:52 +0900

    update self.rb

--- ruby-tutorial/7-chapter/self.rb
+++ ruby-tutorial/7-chapter/self.rb
@@ -22,4 +22,22 @@ user.hello#=> "Hello, I am Alice."
 user.hi#=> "Hi, I am Alice."
 user.my_name#=> "My name is Alice."
 
+class Foo
+    #このputsはクラスの定義の読み込み時に呼び出される
+    puts "クラス構文直下のself: #{self}"
+    def self.bar
+        puts "クラスメソッド内のself: #{self}"
+    end
+    def baz
+        puts "インスタンスメソッド内のself: #{self}"
+    end
+end
+ #=> クラス構文直下のself: Foo
+Foo.bar#=> クラスメソッド内のself: Foo
+foo = Foo.new
+foo.baz#=> インスタンスメソッド内のself: #<Foo:0x000055e981d60400>
+=begin
+クラス構文直下とクラスメソッド内のselfはFooクラス自身を表示しています。インスタンスメソッドはFooクラスのインスタンスを表示している。
+だから、インスタンスメソッド内でクラスメソッドを呼び出す事は出来ない、またその逆も然り
+=end
 
