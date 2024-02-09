c0ef4ac9a3d4b2063fc29d5592fc4406b5358bdf
ChikaraHanakawa <hanakawa.c@gmail.com>
2024-02-09 13:56:47 +0900

    upload file

--- ruby-tutorial/8-chapter/module.rb
+++ ruby-tutorial/8-chapter/module.rb
@@ -34,3 +34,34 @@ class Product
 end
 Product.create_products([])
 Product.tag("deteruyo")
+
+Array.include?(Enumerable)#=> true
+Hash.include?(Enumerable)#=> true
+Range.include?(Enumerable)#=> true
+
+#UFO演算子
+2 <=> 1#=> 1
+2 <=> 1#=> 0
+1 <=> 2#=> -1
+class Tempo
+    include Comparable
+    attr_reader :bpm
+    def initialize(bpm)
+        @bpm = bpm
+    end
+    def <=>(other)
+        other.is_a?(Tempo) ? bpm <=> other.bpm : nil
+    end
+    def inspect
+        "#{bpm}bpm"
+    end
+end
+t_120 = Tempo.new(120)
+t_180 = Tempo.new(180)
+t_120 > t_180
+t_120 < t_180
+t_120 == t_180
+tempos = [Tempo.new(180),Tempo.new(60),Tempo.new(120)]
+pp tempos.sort#=> [60bps, 120bps, 180bps]
+
+
---
+++ ruby-tutorial/8-chapter/toplevel.rb
@@ -0,0 +1,9 @@
+#top level
+p self#=> self
+p self.class#=> Object
+class Tekitou
+    p self
+    p self.class
+end
+
+
